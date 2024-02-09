7d955fd7a4abed232ff6b68511737da45489d809
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2024-02-08 23:42:49 -0300

    [UPDATE] comments

--- 45-asincronia-even-loop.js
+++ 45-asincronia-even-loop.js
@@ -9,27 +9,41 @@ Conceptos importantes:
 * Operaciones Síncronas y Asíncronas
 */
 
+/** Código Síncrono Bloqueante */
+/** Código Asíncrono No Bloqueante */
+
 // Función síncrona bloqueante
+// Inmediatamente se invoca la función anónima, empezando la secuencia síncrona.
 (() => {
-  console.log("Código síncrono");
-  console.log("Inicio");
+  console.log("Código síncrono"); // Primero, imprime "Código síncrono".
+  console.log("Inicio"); // Segundo, imprime "Inicio".
 
+  // Se define la función 'dos', pero no se ejecuta aún.
   function dos() {
-    console.log("Dos");
+    console.log("Dos"); // Cuando se llame a 'dos', imprimirá "Dos".
   }
 
+  // Se define la función 'uno', pero no se ejecuta aún.
   function uno() {
-    console.log("Uno");
-    dos();
-    console.log("Tres");
+    console.log("Uno"); // Cuando se llame a 'uno', empezará por imprimir "Uno".
+    dos(); // Luego, 'uno' llama a 'dos', que imprime "Dos".
+    console.log("Tres"); // Finalmente, después de que 'dos' se ha ejecutado, imprime "Tres".
   }
 
-  uno();
-  console.log("Fin");
+  uno(); // Se llama a la función 'uno', que inicia su secuencia de ejecución.
+  console.log("Fin"); // Después de que 'uno' y 'dos' se han ejecutado, se imprime "Fin".
 })();
 
-// Función asíncrona no bloqueante
+/** La salida en la consola sería:
+Código síncrono
+Inicio
+Uno
+Dos
+Tres
+Fin
+ */
 
+// Función asíncrona no bloqueante
 (() => {
   console.log("Código Asíncrono");
   console.log("Inicio");
@@ -52,3 +66,13 @@ Conceptos importantes:
   uno();
   console.log("Fin");
 })();
+
+/** La salida en la consola sería:
+Código Asíncrono
+Inicio
+Tres
+Fin
+Uno
+Dos
+
+ */
